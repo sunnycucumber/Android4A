5d4e89cef1b63723b92ae3a5dd00351a7ea94bfe
sunnycucumber <guigui.nevoux@gmeil.com>
2020-09-09 09:29:06 +0200

    Initial commit

---
+++ app/src/main/java/com/vogella/android/android4a/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.vogella.android.android4a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+
+
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
